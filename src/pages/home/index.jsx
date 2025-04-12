00f94acb8770ad74a434319717e8839a592f7148
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2025-04-12 20:35:04 +0700

    add password to home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '../../components/common/button'
 import { history } from '../../configs/history'
 import gameService from '../../services/game.service'
@@ -6,6 +6,8 @@ import gameService from '../../services/game.service'
 export const HomePage = () => {
   const [numberOfTeams, setNumberOfTeams] = useState(1)
   const [answerTimeout, setAnswerTimeout] = useState(120)
+  const [password, setPassword] = useState('')
+  const [inputPassword, setInputPassword] = useState('')
 
   const handleCreateGame = () => {
     console.log('numberOfTeams', numberOfTeams)
@@ -37,6 +39,51 @@ export const HomePage = () => {
       }
     })
   }
+
+  const handleCheckPassword = () => {
+    if (inputPassword === 'zxczxc123') {
+      setPassword(inputPassword)
+      localStorage.setItem('password', inputPassword)
+    } else {
+      alert('Mật khẩu không đúng')
+    }
+  }
+
+  useEffect(() => {
+    localStorage.getItem('password') ? setPassword(localStorage.getItem('password')) : setPassword('')
+  }, [])
+
+  if (password === '')
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 8,
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh'
+        }}
+      >
+        <h1>Vui lòng nhập mật khẩu để vào game</h1>
+        <input
+          type='password'
+          value={inputPassword}
+          onChange={(e) => setInputPassword(e.target.value)}
+          placeholder='Nhập mật khẩu'
+          style={{
+            width: 800,
+            height: 64,
+            lineHeight: 1.5,
+            fontSize: 20,
+            border: '1px solid #ccc',
+            paddingLeft: 10,
+            fontSize: 32
+          }}
+        />
+        <Button onClick={handleCheckPassword}>Lưu mật khẩu</Button>
+      </div>
+    )
 
   return (
     <div>
